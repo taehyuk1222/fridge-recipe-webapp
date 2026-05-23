f1a79d29b2f9baf3203cf139fb88f2338a735afd
성윤 <rlarlawldms7503@gmail.com>
2026-05-23 14:12:06 +0900

    feat: 보유 재료 기반 레시피 추천 알고리즘 구현 #7

---
+++ recommend_basic.js
@@ -0,0 +1,15 @@
+import { RECIPE_DATABASE } from './data/recipes.js';
+
+// 현재 내 냉장고에 뭐가 있는지 확인해보자
+export const getRecommendedRecipes = (myIngredients) => {
+    console.log("냉장고 확인:", myIngredients);
+
+    const result = [];
+
+    // 모든 래시피를 하나씩 확인해보자
+    for (let i = 0; i < RECIPE_DATABASE.length; i++) {
+        result.push(RECIPE_DATABASE[i]);
+    }
+
+    return result;
+};
